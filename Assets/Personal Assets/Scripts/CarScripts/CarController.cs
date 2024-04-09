51dbd90275e930e2662ecfbf358183a3a53c81a3
Pedro Augusto Ikeoka <pedroikeoka@gmail.com>
2024-04-09 00:52:49 -0300

    Reworking Car Mechanics

---
+++ Assets/Personal Assets/Scripts/CarScripts/CarController.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarController : MonoBehaviour {
+    [SerializeField]
+    private WheelCollider frontRight;
+    [SerializeField]
+    private WheelCollider frontLeft;
+    [SerializeField]
+    private WheelCollider rearRight;
+    [SerializeField]
+    private WheelCollider rearLeft;
+    private PlayerInput playerInput;
+
+    #region Car Attributes
+
+    [SerializeField]
+    private float motorForce;
+    [SerializeField]
+    private float brakeForce;
+    [SerializeField]
+    private float maxSteerAngle;
+
+    #endregion
+
+
+
+    private void Awake() {
+        playerInput = GetComponent<PlayerInput>();
+
+        IngameActions inputActions = new IngameActions();
+        inputActions.Car.Enable();
+        //inputActions.UI.Enable();
+
+
+    }
+
+    private void FixedUpdate() {
+        Debug.Log(rearLeft.rpm);
+    }
+
+    public void Drive(InputAction.CallbackContext context) {
+        if (context.performed) {
+            Debug.Log("Driving " + context.phase + " " + context.interaction);
+            rearLeft.motorTorque = motorForce * 100;
+            rearRight.motorTorque = motorForce * 100;
+        }
+        if (context.canceled) {
+            Debug.Log("Stopped");
+            rearLeft.motorTorque = 0;
+            rearRight.motorTorque = 0;
+        }
+    }
+
+    public void Brake(InputAction.CallbackContext context) {
+        if (context.performed) {
+            rearLeft.brakeTorque = brakeForce * 100;
+            rearRight.brakeTorque = brakeForce * 100;
+        }
+        if (context.canceled) {
+            rearLeft.brakeTorque = 0;
+            rearRight.brakeTorque = 0;
+        }
+    }
+
+    public void Steering(InputAction.CallbackContext context) {
+        Vector2 direction = context.ReadValue<Vector2>();
+        if (direction != Vector2.zero) {
+            Debug.Log(direction);
+        }
+
+        if (context.started) {
+            frontRight.steerAngle = direction.x * maxSteerAngle;
+            frontLeft.steerAngle = direction.x * maxSteerAngle;
+        }
+
+        if (context.canceled) {
+            frontRight.steerAngle = 0;
+            frontLeft.steerAngle = 0;
+        }
+    }
+
+    public void Reverse(InputAction.CallbackContext context) {
+        if (context.performed) {
+            rearLeft.motorTorque = -motorForce * 60;
+            rearRight.motorTorque = -motorForce * 60;
+        }
+
+        if (context.canceled) {
+            rearLeft.motorTorque = 0;
+            rearRight.motorTorque = 0;
+        }
+
+    }
+
+}
